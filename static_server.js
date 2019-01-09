8bca66b08f350f3298adb841855a35d7f082aa81
hdrpknc <hdrpknc@users.noreply.github.com>
2019-01-09 12:29:58 +0100

    Update static_server.js

--- static_server.js
+++ static_server.js
@@ -7,7 +7,7 @@ app.use(express.static('./'));
 
 var httpProxy = require('http-proxy');
 var proxy = httpProxy.createProxyServer({
- target: "http://10.255.30.134:8034"
+ target: "http://101.255.30.134:8034"
 });
 var resourceProxy = httpProxy.createProxyServer({
  target: "https://sapui5.hana.ondemand.com/1.52.8/resources/sap-ui-core.js"
